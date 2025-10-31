3b584f8851c07feaca731a4d3b0f717bd0bf476f
ssfortynine <ssfortynine@gmail.com>
2025-10-31 18:28:07 +0800

    Update conform-nix

--- config/utils/conform-nvim.nix
+++ config/utils/conform-nvim.nix
@@ -1,75 +1,64 @@
 { pkgs, ... }:
 {
-    extraPackages = with pkgs; [
-      beautysh
-      goimports-reviser
-      nixfmt-rfc-style
-      prettierd
-      ruff
-      sleek
-      stylua
-    ];
+  # Dependencies
+  #
+  # https://nix-community.github.io/nixvim/NeovimOptions/index.html#extrapackages
+  extraPackages = with pkgs; [
+    # Used to format Lua code
+    stylua
+  ];
 
-    plugins.conform-nvim = {
-      enable = true;
-
-      settings = {
-        formatters_by_ft = {
-          astro = [ "prettierd" ];
-          bash = [ "beautysh" ];
-          css = [ "prettierd" ];
-          cpp = [ "clang_format" ];
-          go = [
-            "gofmt"
-            "goimports-reviser"
-          ];
-          html = [ "prettierd" ];
-          javascript = [ "prettierd" ];
-          json = [ "prettierd" ];
-          less = [ "deno_fmt" ];
-          lua = [ "stylua" ];
-          nix = [ "nixfmt" ];
-          python = [
-            # To fix auto-fixable lint errors.
-            "ruff_fix"
-            # To run the Ruff formatter.
-            "ruff_format"
-            # To organize the imports.
-            "ruff_organize_imports"
-          ];
-          sql = [ "sleek" ];
-          tsx = [ "prettierd" ];
-          typescript = [ "prettierd" ];
-          yaml = [ "prettierd" ];
-          zsh = [ "beautysh" ];
-        };
-
-        format_on_save = {
-          lspFallback = true;
-          timeoutMs = 500;
-        };
+  # Autoformat
+  # https://nix-community.github.io/nixvim/plugins/conform-nvim.html
+  plugins.conform-nvim = {
+    enable = true;
+    settings = {
+      notify_on_error = false;
+      format_on_save = ''
+        function(bufnr)
+          -- Disable "format_on_save lsp_fallback" for lanuages that don't
+          -- have a well standardized coding style. You can add additional
+          -- lanuages here or re-enable it for the disabled ones.
+          local disable_filetypes = { c = true, cpp = true }
+          if disable_filetypes[vim.bo[bufnr].filetype] then
+            return nil
+          else
+            return {
+              timeout_ms = 500,
+              lsp_format = "fallback",
+            }
+          end
+        end
+      '';
+      formatters_by_ft = {
+        lua = [ "stylua" ];
+        # Conform can also run multiple formatters sequentially
+        # python = [ "isort "black" ];
+        #
+        # You can use 'stop_after_first' to run the first available formatter from this list
+        #javascript = {
+        # __unkeyed-1 = "prettierd";
+        # __unkeyed-2 = "prettier";
+        # stop_after_first = true;
+        #};
       };
     };
+  };
 
-    keymaps = [
-      {
-        action = {
-          __raw = ''
-            function()
-              require("conform").format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-              })
-            end
-          '';
-        };
-        key = "<leader>mp";
-        mode = [
-          "n"
-          "v"
-        ];
-      }
-    ];
+  # https://nix-community.github.io/nixvim/keymaps/index.html
+  keymaps = [
+    {
+      mode = "";
+      key = "<leader>f";
+      action.__raw = ''
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end
+      '';
+      options = {
+        desc = "[F]ormat buffer";
+      };
+    }
+  ];
 }
 

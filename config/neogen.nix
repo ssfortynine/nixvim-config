4bff7447a2f75c0ca41ca7f77c0505832dfba0c8
ssfortynine <ssfortynine@gmail.com>
2025-10-31 18:16:10 +0800

    Update neogen

--- config/neogen.nix
+++ config/neogen.nix
@@ -1,5 +1,29 @@
 {
   plugins.neogen = {
     enable = true;
+    keymapsSilent = true;
+    keymaps = {
+      generate = "<leader>d";
+    };
+
+    settings = {
+      enable_placeholders = true;
+      snippet_engine = "luasnip";
+      placeholder_hl = "DiagnosticHint";
+      placeholders_text = {
+        args = "[TODO:args]";
+        attribute = "[TODO:attribute]";
+        class = "[TODO:class]";
+        description = "[TODO:description]";
+        kwargs = "[TODO:kwargs]";
+        parameter = "[TODO:parameter]";
+        return = "[TODO:return]";
+        throw = "[TODO:throw]";
+        tparam = "[TODO:tparam]";
+        type = "[TODO:type]";
+        varargs = "[TODO:varargs]";
+      };
+    };
   };
 }
+

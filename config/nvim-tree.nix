0c2297960a0d7880d2d97982fdb7232edb2c3f46
ssfortynine <ssfortynine@gmail.com>
2025-10-31 16:47:06 +0800

    Change neo-tree

--- config/nvim-tree.nix
+++ config/nvim-tree.nix
@@ -1,10 +1,33 @@
 {
-  plugins.neo-tree.settings = {
+  # Neo-tree is a Neovim plugin to browse the file system
+  # https://nix-community.github.io/nixvim/plugins/neo-tree/index.html?highlight=neo-tree#pluginsneo-treepackage
+  plugins.neo-tree = {
     enable = true;
-    filesystem.filteredItems = {
-      hideDotfiles = false;
-      visible = true;
-      hideGitignored = true;
+
+    filesystem = {
+
+      filteredItems = {
+        hideDotfiles = false;
+        visible = true;
+        hideGitignored = true;
+      };
+
+      window = {
+        mappings = {
+          "\\" = "close_window";
+        };
+      };
     };
   };
+
+  # https://nix-community.github.io/nixvim/keymaps/index.html
+  keymaps = [
+    {
+      key = "\\";
+      action = "<cmd>Neotree reveal<cr>";
+      options = {
+        desc = "NeoTree reveal";
+      };
+    }
+  ];
 }
